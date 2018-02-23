41cba6d54a264b1297578712a201ca384c9a9b16
Kai Marshland <kaimarshland@gmail.com>
2018-02-22 18:18:31 -0800

    Added basic style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def menu_item(name, path, opts={})
+    is_current = current_page? path
+
+    "<li class=\"#{is_current ? 'active' : ''}\">#{block_given? ? yield : link_to(name, path, opts)}</li>".html_safe
+  end
+
 end
