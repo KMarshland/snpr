98799eba6628e05b85fb27ca53965da95d7b65e5
Kai Marshland <kaimarshland@gmail.com>
2018-02-22 21:01:37 -0800

    Let it import documents correctly

--- app/models/document.rb
+++ app/models/document.rb
@@ -30,7 +30,85 @@ class Document < ApplicationRecord
   validates :file_url, presence: true
 
   def import
+    cache_location = Rails.root.join('tmp', 'dataset-' + self.file_url.gsub(/\W/, '-') + '.cached')
 
+    if File.exist? cache_location
+      content = File.read cache_location
+
+      puts "Document ID #{self.id}: read from cache"
+    else
+      puts "Document ID #{self.id}: downloading #{self.file_url}"
+
+      response = HTTP.get(self.file_url)
+
+      raise "Invalid response code #{response.code}" unless response.code.to_i == 200
+
+      content = response.body.to_s
+      File.write(cache_location, content)
+
+      puts "Document ID #{self.id}: downloaded #{self.file_url}"
+    end
+
+    lines = content.split("\n")
+
+    puts "Document ID #{self.id}: processing..."
+
+    header = nil
+    whitelist = %w(rsid chromosome position allele1 allele2)
+
+    checked = 0
+    created = 0
+    total_lines = lines.count
+    start_time = DateTime.now
+
+    snp_ids = []
+
+    lines.each do |line|
+      next if line.blank?
+
+      if line.start_with? '#'
+        if header.blank? && line =~ /#\s+(\w+\t){1,5}\w+/
+          header = line.split(/#\s+/).last.split("\t").map(&:strip)
+        end
+
+        next
+      end
+
+      data = line.split("\t").map(&:strip)
+
+      if header.blank? && data[0] == 'rsid'
+        header = data
+        next
+      end
+
+      params = header.zip(data).to_h.slice(*whitelist)
+
+      snp = Snp.where(rsid: params['rsid']).first_or_create do |snp|
+        params.each do |key, value|
+          snp.send("#{key}=", value)
+        end
+
+        created += 1
+      end
+
+      snp.sources += [self.source]
+
+      checked += 1
+
+      if checked % 1000 == 0
+        amount_done = checked/total_lines.to_f
+        percent_done = (100 * amount_done).round(2)
+
+        time_elapsed = ((DateTime.now - start_time).to_f * 1.day).round(2)
+        time_remaining = (time_elapsed / amount_done - time_elapsed).round(2)
+
+        puts "\tDocument ID #{self.id}: ~#{percent_done}% done (#{time_elapsed}s elapsed, ~#{time_remaining}s remaining)"
+      end
+    end
+
+    puts "Processed document ID #{self.id}: #{created} new SNPs created (#{checked} in document, #{total_lines} lines)"
+
+    self.update(imported: true)
   end
 
   def as_json(_opts={})
